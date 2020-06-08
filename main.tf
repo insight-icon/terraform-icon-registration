087e2233c010f588c55462e19673edcc25b1d483
rob <robcannonxyz@gmail.com>
2020-06-07 20:47:43 -0700

    removing label module

--- main.tf
+++ main.tf
@@ -13,21 +13,6 @@ locals {
   details_endpoint = "${var.static_endpoint}/details.json"
 }
 
-module "label" {
-  source = "github.com/robc-io/terraform-null-label.git?ref=0.16.1"
-
-  tags = {
-    NetworkName = var.network_name
-    Owner       = var.owner
-    Terraform   = true
-    VpcType     = "main"
-  }
-
-  environment = var.environment
-  namespace   = var.namespace
-  stage       = var.stage
-}
-
 ###########
 # Templates
 ###########
